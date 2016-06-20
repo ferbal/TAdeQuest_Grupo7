5a48c4c242573895a93a1671676c5a6dd430d638
Santiago Foster <santiagofrf@gmail.com>
2016-06-20 17:41:53 -0300

    modelado de misiones/tareas

--- TAdeQuest/src/tadp/Mision.scala
+++ TAdeQuest/src/tadp/Mision.scala
@@ -2,4 +2,14 @@ package tadp
 
 class Mision (tareas: List[Tarea] = List[Tarea](), recompensa: Equipo => Equipo){
   
+  def realizarMision(equipo:Equipo): Equipo = {
+    
+    try{
+    recompensa(tareas.foldLeft(equipo){(x, y) => y.realizarTarea(x)})
+    }catch{
+      case _ => equipo
+    }
+    
+  }
+  
 }
