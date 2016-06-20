120e09c2d3c0151a8e4b900462531d07c8cdd89d
Santiago Foster <santiagofrf@gmail.com>
2016-06-20 14:07:44 -0300

    Cambios a Equipo

--- TAdeQuest/src/tadp/Equipo.scala
+++ TAdeQuest/src/tadp/Equipo.scala
@@ -1,26 +1,43 @@
 package tadp
 
-case class Equipo (heroes: List[Heroe] = List[Heroe]() , oro: Int, nombre:String) {
-  
-  def mejorHeroeSegun(f:Heroe=> Int): Heroe ={
-    heroes.maxBy { x => f(x) }
+import scala.util.control.Exception
+
+case class Equipo(heroes: List[Heroe] = List[Heroe](), oro: Int, nombre: String) {
+
+  def mejorHeroeSegun(f: Heroe => Int): List[Heroe] = {
+    heroes match {
+      case x :: Nil => x::Nil
+      case x :: xs =>
+        val results = heroes.map { x => f(x) }
+        val max = results.reduceLeft(_ max _)
+        heroes.filter { x => f(x) == max }
+      case Nil => Nil
+    }
   }
-  
-  def obtenerItem(item: Item){
-    mejorHeroeSegun({x=> 
+
+  def obtenerItem(item: Item): Equipo = {
+    mejorHeroeSegun({ x =>
       val statnuevo = x.utilizar_item(item.ubicacion, item).get_stat_principal
-      statnuevo - x.get_stat_principal})
+      statnuevo - x.get_stat_principal
+    }) match {
+      case Nil => throw new Exception("No hay heroes aptos para equiparse el item")
+      case x::xs => reemplazarMiembro(x, x.utilizar_item(item.ubicacion, item))
+    }
   }
-  
-  def obtenerMiembro(heroe:Heroe): Equipo = {
-    copy(heroes = heroes.::(heroe))
+
+  def obtenerMiembro(heroe: Heroe): Equipo = {
+    copy(heroes = heroe :: heroes)
   }
-  
-  def reemplazarMiembro(reemplazado:Heroe,reemplazo:Heroe): Equipo = {
+
+  def reemplazarMiembro(reemplazado: Heroe, reemplazo: Heroe): Equipo = {
     copy(heroes.updated(heroes.indexOf(reemplazado), reemplazo))
   }
-  
+
   def lider: Heroe = {
-    mejorHeroeSegun({x=> x.get_stat_principal})
+    mejorHeroeSegun({ x => x.get_stat_principal }) match {
+      case x :: Nil => x
+      case x :: xs  => throw new Exception("Hay mas de un Lider, por lo tanto nadie es Lider")
+      case Nil      => throw new Exception("No hay Lider")
+    }
   }
 }
