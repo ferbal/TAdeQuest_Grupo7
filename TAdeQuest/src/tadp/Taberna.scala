8a88eea727c3998b877eb0e2df34b3676e0b40b1
DiegoQuirosUTN <diegoaquiros@hotmail.com>
2016-06-20 18:53:11 -0300

    elegitProximaMision andando

--- TAdeQuest/src/tadp/Taberna.scala
+++ TAdeQuest/src/tadp/Taberna.scala
@@ -9,19 +9,17 @@ class Taberna {
  }
  
  def mejorMisionSegunCriterio(equipo:Equipo, criterio:(Equipo,Equipo) => Boolean, mision1:Mision, mision2:Mision):Mision={
-   if (criterio(mision1.realizarMision(equipo),mision2.realizarMision(equipo))
-       {return mision1}
+   if (criterio(mision1.realizarMision(equipo),mision2.realizarMision(equipo)))
+       return mision1
    else
-       {return mision2}
+       return mision2
  }
  
  def elegirMision(equipo: Equipo, criterio: (Equipo,Equipo) => Boolean): Mision ={
    
-   val comparador = mejorMisionSegunCriterio equipo criterio _ _ 
-   val resultado = misiones.reduceLeft()
-   
 
+   misiones.reduceLeft({(x,y)=> mejorMisionSegunCriterio(equipo, criterio, x,y)})
    
- }
+}
   
 }
