e39110ccc4c80a3fda5c5d8aa504fae54120bf0c
Santiago Foster <santiagofrf@gmail.com>
2016-06-24 18:27:15 -0300

    Funcionalidad de entrenar equipos

--- TAdeQuest/src/tadp/Tarea.scala
+++ TAdeQuest/src/tadp/Tarea.scala
@@ -4,21 +4,18 @@ import scala.util.Try
 
 class Tarea(consecuencias: Heroe => Heroe, facilidad: (Equipo, Heroe) => Try[Int]) {
 
-  
-  def realizarTareaRecibeTry(equipo: Try[Equipo]): Try[Equipo] = {
-    for{
+  def realizarTarea(equipo: Try[Equipo]): Try[Equipo] = {
+    for {
       eq <- equipo
       nuevoeq <- realizarTarea(eq)
-    }yield(nuevoeq)
+    } yield (nuevoeq)
   }
+  
   def realizarTarea(equipo: Equipo): Try[Equipo] = {
-
-    
-
     Try(
-    equipo.mejorHeroeSegun { x => facilidad(equipo, x) } match {
-      case x :: xs => equipo.reemplazarMiembro(x, consecuencias(x))
-      case Nil     => throw new Exception("No hay ningun heroe capaz de realizar la tarea")
-    })
+      equipo.mejorHeroeSegun { x => facilidad(equipo, x) } match {
+        case x :: xs => equipo.reemplazarMiembro(x, consecuencias(x))
+        case Nil     => throw new Exception("No hay ningun heroe capaz de realizar la tarea")
+      })
   }
 }
